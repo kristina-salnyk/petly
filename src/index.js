7520f70dd2b674641d69f2648e1f78d7ed03d2c7
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-17 13:16:38 +0000

    fixed normalize bug

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import './index.css';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
