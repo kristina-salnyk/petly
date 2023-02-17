7b5654f366935a329ab991fa198154aa778fbb42
Vitalii Aleksandrov <vitaliy0508@ukr.net>
2023-02-18 01:03:37 +0200

    add partial styles for UserPage

--- src/components/UserData/UserData.jsx
+++ src/components/UserData/UserData.jsx
@@ -1,54 +1,67 @@
+import { Logout } from '../../components/Logout/Logout';
+import {
+  MyInformation,
+  InformationBackgroundBlock,
+  ProfileImgWrapper,
+  ProfileImg,
+} from './UserData.styled';
+import { ReactComponent as CameraIcon } from '../../icons/cameraIcon.svg';
 import defaultImage from '../../images/ellipse.png';
+
 export const UserData = () => {
   const imageUrl = defaultImage;
 
   return (
     <div>
-      <p>
-        My information:
-        <div>
-          <img src={imageUrl} alt="User avatar" />
-          <button type="button"></button>
-          <p>Edit photo</p>
-          <ul>
-            <li>
-              <label>
-                Name:
-                <input type="text" />
-              </label>
-              <button type="button"></button>
-            </li>
-            <li>
-              <label>
-                Email:
-                <input type="text" />
-              </label>
-              <button type="button"></button>
-            </li>
-            <li>
-              <label>
-                Birthday:
-                <input type="text" />
-              </label>
-              <button type="button"></button>
-            </li>
-            <li>
-              <label>
-                Phone:
-                <input type="text" />
-              </label>
-              <button type="button"></button>
-            </li>
-            <li>
-              <label>
-                City:
-                <input type="text" />
-              </label>
-              <button type="button"></button>
-            </li>
-          </ul>
-        </div>
-      </p>
+      <MyInformation>My information:</MyInformation>
+      <InformationBackgroundBlock>
+        <ProfileImgWrapper>
+          <ProfileImg src={imageUrl} alt="User avatar" />
+        </ProfileImgWrapper>
+
+        <button type="button">
+          <CameraIcon />
+          <span>Edit photo</span>
+        </button>
+        <ul>
+          <li>
+            <label>
+              Name:
+              <input type="text" />
+            </label>
+            <button type="button"></button>
+          </li>
+          <li>
+            <label>
+              Email:
+              <input type="text" />
+            </label>
+            <button type="button"></button>
+          </li>
+          <li>
+            <label>
+              Birthday:
+              <input type="text" />
+            </label>
+            <button type="button"></button>
+          </li>
+          <li>
+            <label>
+              Phone:
+              <input type="text" />
+            </label>
+            <button type="button"></button>
+          </li>
+          <li>
+            <label>
+              City:
+              <input type="text" />
+            </label>
+            <button type="button"></button>
+          </li>
+        </ul>
+        <Logout />
+      </InformationBackgroundBlock>
     </div>
   );
 };
