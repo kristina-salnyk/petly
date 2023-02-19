a9d3c38bbc4b1d27c887d7922e7678a8236bbee3
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-19 00:25:34 +0000

    add svg directory to see what you add

--- src/components/icons/FavoriteHeartIcon.jsx
+++ src/components/icons/FavoriteHeartIcon.jsx
@@ -3,32 +3,63 @@ import PropTypes from 'prop-types';
 import theme from '../../utils/theme';
 
 export const FavoriteHeartIcon = ({
-  width = 17,
-  heigth = 18,
-  color = theme.colors.accent,
+  size = 28,
+  borderColor = theme.colors.accent,
+  insideColor = theme.colors.white,
   ...props
 }) => (
-  <svg width={width} height={heigth} fill="none" viewBox="0 0 17 18" {...props}>
-    <g clipPath="url(#a)">
-      <path
-        d="M5.167 2C3.326 2 1.833 3.477 1.833 5.3c0 1.471.584 4.963 6.326 8.493a.657.657 0 0 0 .682 0c5.742-3.53 6.326-7.022 6.326-8.493 0-1.823-1.493-3.3-3.334-3.3C9.993 2 8.5 4 8.5 4S7.007 2 5.167 2Z"
-        fill={color}
-        stroke={color}
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+  <svg width={size} height={size} fill="none" viewBox="0 0 28 28" {...props}>
+    <g filter="url(#a)">
+      <g filter="url(#b)">
+        <path
+          d="M8 3C4.687 3 2 5.734 2 9.106c0 2.723 1.05 9.185 11.386 15.717a1.158 1.158 0 0 0 1.228 0C24.95 18.29 26 11.829 26 9.106 26 5.734 23.313 3 20 3s-6 3.7-6 3.7S11.313 3 8 3Z"
+          fill={insideColor}
+          fillOpacity={0.6}
+        />
+        <path
+          d="M8 3C4.687 3 2 5.734 2 9.106c0 2.723 1.05 9.185 11.386 15.717a1.158 1.158 0 0 0 1.228 0C24.95 18.29 26 11.829 26 9.106 26 5.734 23.313 3 20 3s-6 3.7-6 3.7S11.313 3 8 3Z"
+          stroke={borderColor}
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
     </g>
     <defs>
-      <clipPath id="a">
-        <path fill={theme.colors.white} transform="translate(.5)" d="M0 0h16v16H0z" />
-      </clipPath>
+      <filter
+        id="a"
+        x={-100}
+        y={-100}
+        width={228}
+        height={228}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feGaussianBlur in="BackgroundImageFix" stdDeviation={50} />
+        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_120_584" />
+        <feBlend in="SourceGraphic" in2="effect1_backgroundBlur_120_584" result="shape" />
+      </filter>
+      <filter
+        id="b"
+        x={-3}
+        y={-2}
+        width={34}
+        height={32}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feGaussianBlur in="BackgroundImageFix" stdDeviation={2} />
+        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_120_584" />
+        <feBlend in="SourceGraphic" in2="effect1_backgroundBlur_120_584" result="shape" />
+      </filter>
     </defs>
   </svg>
 );
 
 FavoriteHeartIcon.propTypes = {
-  color: PropTypes.string,
-  width: PropTypes.number,
-  heigth: PropTypes.number,
+  borderColor: PropTypes.string,
+  insideColor: PropTypes.string,
+  size: PropTypes.number,
 };
