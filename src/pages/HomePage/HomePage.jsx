ba279108cf5f86075d9d3f94bbf23ea02b979d99
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-19 13:21:29 +0000

    fix deploy

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,21 +4,23 @@ import WaveDesk from '../../components/Header/images/waveDesk.svg';
 import UnionDesk from '../../components/Header/images/UnionDesk.svg';
 import girl from '../../components/Header/images/girl.svg';
 import {Waves, IMG, H1, Page, WavesDesk, Union} from '../../components/Header/Header.styled';
+import {NavLink} from 'react-router-dom';
 
 const HomePage = () => {
-  return (
+  return (<div>
+    <NavLink to="/news">Home</NavLink>
 
-    <>
-      <Page>
-        <H1>Take good care of <br/>your small pets</H1>
-      </Page>
-      <div>
-        <IMG src={girl} alt="girl"></IMG>
-        <Waves src={WaveMob} alt="waves"></Waves>
-        <WavesDesk src={WaveDesk}></WavesDesk>
-        <Union src={UnionDesk}></Union>
-      </div>
-    </>
+    <Page>
+      <H1>Take good care of <br/>your small pets</H1>
+    </Page>
+    <div>
+      <IMG src={girl} alt="girl"></IMG>
+      <Waves src={WaveMob} alt="waves"></Waves>
+      <WavesDesk src={WaveDesk}></WavesDesk>
+      <Union src={UnionDesk}></Union>
+    </div>
+  </div>
+
 
   ) 
 
