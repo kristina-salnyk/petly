e80a0c5d72834619cb8fe805d5d311a42f8c2d8b
Vitalii Aleksandrov <vitaliy0508@ukr.net>
2023-02-19 21:26:23 +0200

    add UserPage desktop markup with styles

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,7 +1,13 @@
+// import { LogoutIcon } from '../../components/icons/LogoutIcon';
+import { LogoutBtn, LogoutSpan } from './Logout.styled';
+import { ReactComponent as LogoutIcon } from '../../icons/logout.svg';
 export const Logout = () => {
   return (
     <>
-      <button type="button">Log Out</button>
+      <LogoutBtn type="button">
+        <LogoutIcon />
+        <LogoutSpan>Log Out</LogoutSpan>
+      </LogoutBtn>
     </>
   );
 };
--- src/components/UserData/UserData.styled.jsx
+++ src/components/UserData/UserData.styled.jsx
@@ -14,13 +14,53 @@ export const ProfileImgWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  padding-top: 20px;
+`;
+
+export const ProfileImgBtn = styled.button`
+  width: auto;
+  height: auto;
+  margin: 0;
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  border-radius: 50%;
 `;
 
 export const ProfileImg = styled.img`
   width: 233px;
   height: 233px;
+  border-radius: 50%;
+`;
 
-  margin-top: 20px;
+export const PhotoEditButton = styled.button`
+  position: absolute;
+  top: 280px;
+  left: 300px;
+  // top: 60%;
+  // left: 18%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border: 0;
+  border-radius: 4px;
+  cursor: pointer;
+  padding: 4px;
+
+  &:hover {
+    background-color: #fdf7f2;
+  }
+`;
+
+export const PhotoEditSpan = styled.span`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 22px;
+  letter-spacing: 0.04em;
+  color: #111111;
+  margin-left: 4px;
 `;
 
 export const InformationBackgroundBlock = styled.div`
