5b4bd1a1b59ba3bf2a6c013007767701e9ee2574
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-19 15:32:41 +0000

    delete extra code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,12 +4,9 @@ import WaveDesk from '../../components/Header/images/waveDesk.svg';
 import UnionDesk from '../../components/Header/images/UnionDesk.svg';
 import girl from '../../components/Header/images/girl.svg';
 import {Waves, IMG, H1, Page, WavesDesk, Union} from '../../components/Header/Header.styled';
-import {NavLink} from 'react-router-dom';
 
 const HomePage = () => {
   return (<div>
-    <NavLink to="/news">Home</NavLink>
-
     <Page>
       <H1>Take good care of <br/>your small pets</H1>
     </Page>
