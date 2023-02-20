3d560b5f83d612a7d1702aa31cf7092c96c25c19
Vladislav Chornyi <98426939+VladyslavIT@users.noreply.github.com>
2023-02-20 16:52:02 +0300

    add notice card

---
+++ src/components/NoticeCategoryItem/Image/NoticeImage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  ImageCard,
+  InfoWrapper,
+  CategoryLabel,
+  FavoriteLabel,
+  LabelText,
+} from './NoticeImage.styled';
+import { FavoriteHeartIcon } from '../../icons/FavoriteHeartIcon';
+
+export const NoticeImage = () => {
+  return (
+    <>
+      <ImageCard>
+        <InfoWrapper>
+          <CategoryLabel>
+            <LabelText>in good hands</LabelText>
+          </CategoryLabel>
+          <FavoriteLabel>
+            <FavoriteHeartIcon />
+          </FavoriteLabel>
+        </InfoWrapper>
+      </ImageCard>
+    </>
+  );
+};
--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -13,3 +13,4 @@ export const NoticeCategoryItem = () => {
     </NoticeCard>
   );
 };
+// li item
