f4788a7afd99dbed9732341a544b330bf945e542
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-02-20 21:54:20 +0100

    feature(theme): add breakpoints and additional properties

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,13 @@
 const theme = Object.freeze({
+  breakpoints: {
+    mobile: '768px',
+    tablet: ['769px', '1280px'],
+    desktop: '1281px',
+  },
   colors: {
     accent: '#F59256',
     brightAccent: '#FF6101',
+    hoverAccent: '#ffa96f',
     black: '#111111',
     white: '#FFFFFF',
     link: '#3091EB',
@@ -40,6 +46,7 @@ const theme = Object.freeze({
   shape: {
     borderRadius: { s: 8, m: 20, l: 40, xl: 50 },
     borderWidth: 2,
+    opacity: 0.6,
   },
   typography: {
     font: {
