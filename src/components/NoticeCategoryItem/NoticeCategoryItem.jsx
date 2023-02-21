ed4966d207f791f84667648fceb869c51067f7a1
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-02-21 12:11:16 +0100

    feature(notice-item): add test notice markup

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,3 +1,51 @@
-export const NoticeCategoryItem = () => {
-  return <div>NoticeCategoryItem</div>;
+import PropTypes from 'prop-types';
+
+export const NoticeCategoryItem = ({ category, title, birthday, breed, location, image }) => {
+  return (
+    <li>
+      <div style={{ position: 'relative' }}>
+        <span style={{ position: 'absolute', top: 0, left: 0 }}>{category}</span>
+        <div style={{ width: 280, height: 288, marginBottom: 20 }}>
+          <img
+            src={image}
+            alt={title}
+            style={{ width: '100%', display: 'block', objectFit: 'cover' }}
+          />
+        </div>
+      </div>
+      <div>
+        <h3>{title}</h3>
+        <div>
+          <div>
+            <span>Breed:</span>
+            <span>{breed}</span>
+          </div>
+          <div>
+            <span>Place:</span>
+            <span>{location}</span>
+          </div>
+          <div>
+            <span>Age:</span>
+            <span>{birthday}</span>
+          </div>
+        </div>
+        <button type="button">Learn more</button>
+        <button type="button">Delete</button>
+      </div>
+    </li>
+  );
+};
+
+NoticeCategoryItem.propTypes = {
+  _id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  birthday: PropTypes.string,
+  breed: PropTypes.string,
+  gender: PropTypes.string,
+  location: PropTypes.string,
+  price: PropTypes.string,
+  image: PropTypes.string,
+  comments: PropTypes.string,
 };
