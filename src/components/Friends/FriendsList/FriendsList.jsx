48b24396a6c8a4bb121dc3460e6839f66b6437b5
wi2410 <101503219+wi2410@users.noreply.github.com>
2023-02-21 20:47:52 +0200

    add OurFriendsPage

---
+++ src/components/Friends/FriendsList/FriendsList.jsx
@@ -0,0 +1,41 @@
+import { TitleFriends, List } from './FriendsList.styled';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectFriends, selectIsLoading } from '../../../redux/friends/selectors';
+import { fetchFriends } from '../../../redux/friends/operations';
+import FriendsItem from '../FriendsItem/FriendsItem';
+
+
+export const FriendsList = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const friends = useSelector(selectFriends);
+  
+  console.log('FetchFRIENDS', friends)
+  useEffect(() => {
+    dispatch(fetchFriends())
+  }, [dispatch]);
+
+  return (
+    <>
+      <TitleFriends>Our Friends</TitleFriends>
+      <List>
+        {isLoading &&
+                    friends.map(({ _id, title, url, addressUrl, imageUrl, address, workDays, phone, email, }) => (
+                      <FriendsItem
+                        key={_id}
+                        title={title}
+                        website={url}
+                        location={addressUrl}
+                        imageUrl={imageUrl}
+                        address={address}
+                        workDays={workDays}
+                        phone={phone}
+                        email={email}
+                      />))}
+      </List>
+    </>
+        
+  );
+}
