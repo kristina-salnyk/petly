4710561ef63ff9e0278e53c5b43c2e8ffb326d52
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-22 19:41:51 +0000

    return noticepage code

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,50 +1,59 @@
-import PropTypes from 'prop-types';
-import { Form, Input, InputSearchControl, InputWrap } from './NoticesSearch.styled';
-import { SearchIcon } from '../icons/SearchIcon';
-import { CloseSearchCross } from '../icons/CloseSearchCross';
-import { useIconSize } from '../../hooks/useIconSize';
+import { Suspense, useEffect } from 'react';
+import { Outlet, useParams, useSearchParams } from 'react-router-dom';
+import { NoticesSearch } from '../../components/NoticesSearch';
+import { NoticeCategoriesNav } from '../../components/NoticesCategoriesNav/NoticesCategoriesNav';
+import { AddNoticeButton } from '../../components/AddNoticeButton';
+import { Container, ContentWrap, PageTitle, TopPanel } from './NoticesPage.styled';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { selectSearchQuery } from '../../redux/notices/selectors';
+import { fetchNotices } from '../../redux/notices/operations';
 import { changeSearchQuery } from '../../redux/notices/slice';
 
-export const NoticesSearch = ({ onSubmit }) => {
+const NoticesPage = () => {
   const dispatch = useDispatch();
-  const [iconSize] = useIconSize();
+  const { category } = useParams();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryParam = searchParams.get('query') ?? '';
 
   const searchQuery = useSelector(selectSearchQuery);
 
-  const onSubmitSearchQuery = event => {
-    event.preventDefault();
-    onSubmit();
+  useEffect(() => {
+    if (!queryParam) {
+      return setSearchQueryParam();
+    }
+    dispatch(changeSearchQuery(queryParam));
+    return () => {
+      dispatch(changeSearchQuery(''));
+    };
+  }, [queryParam]);
+
+  useEffect(() => {
+    dispatch(fetchNotices({ category }));
+  }, [dispatch, category]);
+
+  const setSearchQueryParam = () => {
+    const query = searchQuery.trim();
+    const newParams = query !== '' ? { query } : {};
+    setSearchParams(newParams);
   };
 
   return (
-    <Form onSubmit={onSubmitSearchQuery}>
-      <InputWrap>
-        <Input
-          type="search"
-          name="searchQuery"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search"
-          value={searchQuery}
-          onInput={event => dispatch(changeSearchQuery(event.target.value))}
-        />
-        <InputSearchControl>
-          {searchQuery ? (
-            <CloseSearchCross
-              size={iconSize === 's' ? 20 : 24}
-              onClick={() => dispatch(changeSearchQuery(''))}
-            />
-          ) : (
-            <SearchIcon size={iconSize === 's' ? 20 : 24} />
-          )}
-        </InputSearchControl>
-      </InputWrap>
-    </Form>
+    <Container>
+      <ContentWrap>
+        <PageTitle>Find your favorite pet</PageTitle>
+        <NoticesSearch onSubmit={setSearchQueryParam} />
+        <TopPanel>
+          <NoticeCategoriesNav />
+          <AddNoticeButton />
+        </TopPanel>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </ContentWrap>
+    </Container>
   );
 };
 
-NoticesSearch.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
+export default NoticesPage;
