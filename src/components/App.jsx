3c8d0709633658cafeaba4c5184fe184544b275f
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-25 13:55:50 +0000

    fix refreshing from notices page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import React, { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 import { NoticeCategoriesList } from './NoticesCategoriesList/NoticesCategoriesList';
-import PrivateRoute from './Routes/PrivateRoute';
-import RestrictedRoute from './Routes/RestrictedRoute';
+import { PrivateRoute } from '../hooks/PrivateRoute';
+import { RestrictedRoute } from '../hooks/RestrictedRoute';
 
 const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
@@ -11,6 +11,7 @@ const NewsPage = lazy(() => import('../pages/NewsPage/NewsPage'));
 const NoticesPage = lazy(() => import('../pages/NoticesPage/NoticesPage'));
 const OurFriendsPage = lazy(() => import('../pages/OurFriendsPage/OurFriendsPage'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+const UserPage = lazy(() => import('../pages/UserPage/UserPage'));
 
 export function App() {
   return (
@@ -18,15 +19,20 @@ export function App() {
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/news" element={<NewsPage />} />
-        <Route path="/register" element={<RestrictedRoute />}>
-          <Route index element={<RegisterPage />} />
-        </Route>
-        <Route path="/login" element={<RestrictedRoute />}>
-          <Route index element={<LoginPage />} />
-        </Route>
+        <Route
+          path="/register"
+          element={<RestrictedRoute component={<RegisterPage />} redirectTo="/user" />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute component={<LoginPage />} redirectTo="/user" />}
+        />
         <Route path="/notices" element={<NoticesPage />} />
         <Route path="/friends" element={<OurFriendsPage />} />
-        <Route path="/user" element={<PrivateRoute />} />
+        <Route
+          path="/user"
+          element={<PrivateRoute component={<UserPage />} redirectTo="/login" />}
+        />
         <Route path="/notices" element={<NoticesPage />}>
           <Route path="" element={<Navigate to="sell" replace />} />
           <Route path=":category" element={<NoticeCategoriesList />} />
