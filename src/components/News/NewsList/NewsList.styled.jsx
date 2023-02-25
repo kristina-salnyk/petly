8eece07e07643bfd935c3fe2631ae1d1334389ab
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-25 21:57:52 +0000

    delete extra coma

--- src/components/News/NewsList/NewsList.styled.jsx
+++ src/components/News/NewsList/NewsList.styled.jsx
@@ -1,43 +1,34 @@
 import styled from 'styled-components';
 
 export const ListNews = styled.ul`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    list-style: none; 
-    @media (min-width: ${({ theme }) => theme.breakpoints.tablet[0]}){
-      padding-top: ${({ theme }) => theme.spacing[5]}px;
-    }
-    
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  list-style: none;
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet[0]}) {
+    padding-top: ${({ theme }) => theme.spacing[5]}px;
+  }
 `;
 
 export const ItemNews = styled.li`
-    flex-basis: calc(100% / 1 - 40px);
-    margin: 20px;
-    display: flex;
-    justify-content: center;
+  flex-basis: calc(100% / 1 - 40px);
+  margin: 20px;
+  display: flex;
+  justify-content: center;
 
-    @media (min-width: ${({ theme }) => theme.breakpoints.tablet[0]}){
-      flex-basis: calc(100% / 2 - 32px);
-      margin: 16px;
-      margin-bottom: 60px;
-    };
+  @media (min-width: ${({ theme }) => theme.breakpoints.tablet[0]}) {
+    flex-basis: calc(100% / 2 - 32px);
+    margin: 16px;
+    margin-bottom: 60px;
+  }
 
-    @media (min-width: ${({ theme }) => theme.breakpoints.desktopMin}){
-      flex-basis: calc(100% / 3 - 32px);
-      margin: 16px;
-    };
-    
+  @media (min-width: ${({ theme }) => theme.breakpoints.desktopMin}) {
+    flex-basis: calc(100% / 3 - 32px);
+    margin: 16px;
+  }
 `;
 
 export const ConteinerNews = styled.div`
-    margin-top: 40px;
-    margin: 0 auto;
-  };
+  margin-top: 40px;
+  margin: 0 auto;
 `;
-
-
-
-
-
-
