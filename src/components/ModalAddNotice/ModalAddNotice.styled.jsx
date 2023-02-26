1b5b1f44a05e56e174c27bc2575eaec236a4465e
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-02-26 13:38:28 +0000

    test

--- src/components/ModalAddNotice/ModalAddNotice.styled.jsx
+++ src/components/ModalAddNotice/ModalAddNotice.styled.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const Background = styled.div`
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.8);
+  /* background: rgba(0, 0, 0, 0.8); */
   position: fixed;
   top: 0;
   left: 0;
