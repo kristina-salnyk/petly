26a273942a9114e2fa6e34de57ab1c7a58d6820e
wi2410 <101503219+wi2410@users.noreply.github.com>
2023-02-28 22:41:36 +0200

    add scroolToTopBtn

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -1,12 +1,13 @@
 import FriendsList from '../../components/Friends/FriendsList/FriendsList';
 import { Container } from './OurFriendsPage.styled';
+import ScrollToTop from '../../components/ScrollToTop/ScrollToTopButton';
 
 const OurFriendsPage = () => {
   return (
     <Container>
       <FriendsList />
+      <ScrollToTop />
     </Container>
-    
   );
 };
 
