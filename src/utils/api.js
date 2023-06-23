c1d4f0e7ce1c912b1a7a9be0271bca5d9103ee91
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-06-23 07:51:44 +0200

    feature(url): update backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const api = axios.create({
   // baseURL: 'http://localhost:8080/api',
   // baseURL: 'https://pet-support.onrender.com/api',
-  baseURL: 'http://3.125.69.59:8080/api',
+  baseURL: 'http://3.125.69.59/api',
 });
 
 export const setAuthHeader = token => {
